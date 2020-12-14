e68d57fa55fd8eb040c3019a38b02f98416660f4
suzukalight <mail@mkubara.com>
2020-12-14 14:11:23 +0900

    Add _document

---
+++ src/pages/_document.tsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import NextDocument, { Html, Head, Main, NextScript } from 'next/document';
+import { ColorModeScript } from '@chakra-ui/react';
+
+export default class Document extends NextDocument {
+  static async getInitialProps(ctx) {
+    const initialProps = await NextDocument.getInitialProps(ctx);
+    return { ...initialProps };
+  }
+
+  render() {
+    return (
+      <Html>
+        <Head />
+        <body>
+          <ColorModeScript />
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    );
+  }
+}
