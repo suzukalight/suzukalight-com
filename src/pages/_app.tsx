c612fbff59dab641a98615a08cdaf3eac3ce3eae
suzukalight <mail@mkubara.com>
2020-12-14 14:11:44 +0900

    Add ColorModeProvider to _app

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,12 +1,17 @@
-import * as React from 'react';
+import React from 'react';
 import { AppProps } from 'next/app';
-import { ChakraProvider, CSSReset } from '@chakra-ui/react';
+import { ChakraProvider, ColorModeProvider } from '@chakra-ui/react';
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <ChakraProvider>
-      <CSSReset />
-      <Component {...pageProps} />
+      <ColorModeProvider
+        options={{
+          useSystemColorMode: true,
+        }}
+      >
+        <Component {...pageProps} />
+      </ColorModeProvider>
     </ChakraProvider>
   );
 }
