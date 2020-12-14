f6deaf01a9f18cd78bc4d17b909edd6de52c3ae8
suzukalight <mail@mkubara.com>
2020-12-15 01:01:40 +0900

    Add ArticleList component

---
+++ src/components/molecules/ArticleList/index.tsx
@@ -0,0 +1,86 @@
+import React from 'react';
+import Link from 'next/link';
+import {
+  Flex,
+  Center,
+  Icon,
+  Heading,
+  Img,
+  Text,
+  SimpleGrid,
+  Link as ChakraLink,
+  Badge,
+} from '@chakra-ui/react';
+import { FaPen } from 'react-icons/fa';
+
+import format from 'date-fns/format';
+
+type ArticleData = {
+  slug: string;
+  title: string;
+  excerpt: string;
+  date: string | null;
+  tags: string[] | null;
+  image?: string;
+};
+
+type ArticleCardProps = ArticleData & { baseUrl: string };
+
+export const ArticleCard: React.FC<ArticleCardProps> = ({
+  slug,
+  title,
+  date,
+  tags,
+  image,
+  baseUrl,
+}) => (
+  <Link href={`${baseUrl}/[slug]`} as={`${baseUrl}/${slug}`}>
+    <ChakraLink>
+      <Flex direction="row" maxH={24} overflowY="hidden">
+        {image ? (
+          <Img
+            src={image}
+            boxSize={24}
+            borderRadius={8}
+            flexShrink={0}
+            mr={4}
+            backgroundColor="gray.100"
+          />
+        ) : (
+          <Center boxSize={24} borderRadius={8} flexShrink={0} mr={4} backgroundColor="gray.100">
+            <Icon as={FaPen} boxSize={12} color="gray.500" />
+          </Center>
+        )}
+
+        <Flex flexGrow={1} direction="column">
+          <Heading as="h3" size="sm" lineHeight={1.5} maxH="3em" overflowY="hidden">
+            {title}
+          </Heading>
+          <Flex flexGrow={1} direction="column" justifyContent="flex-end">
+            <Text fontSize="sm" color="gray.500" opacity="0.8">
+              {date ? format(new Date(date), 'yyyy.MM.dd') : ''}
+            </Text>
+            <Flex direction="row" maxH="1em" overflow="hidden" ml="-4px">
+              {(tags || []).map((tag) => (
+                <Badge key={tag} color="gray.500">{`#${tag}`}</Badge>
+              ))}
+            </Flex>
+          </Flex>
+        </Flex>
+      </Flex>
+    </ChakraLink>
+  </Link>
+);
+
+type ArticleListProps = {
+  articles: ArticleData[];
+  baseUrl: string;
+};
+
+export const ArticleList: React.FC<ArticleListProps> = ({ articles, baseUrl }) => (
+  <SimpleGrid columns={[1, 1, 2]} gap={4}>
+    {articles.map((article) => (
+      <ArticleCard key={article.slug} {...article} baseUrl={baseUrl} />
+    ))}
+  </SimpleGrid>
+);
--- src/pages/blog/index.tsx
+++ src/pages/blog/index.tsx
@@ -4,13 +4,14 @@ import Link from 'next/link';
 import fs from 'fs';
 import path from 'path';
 import matter from 'gray-matter';
-import { Box, Heading, ListItem, UnorderedList, Text, Link as ChakraLink } from '@chakra-ui/react';
+import { Box, Heading, Text, Link as ChakraLink } from '@chakra-ui/react';
+import { ArticleList } from '../../components/molecules/ArticleList';
 
 const root = process.cwd();
 const contentDir = 'contents/blog';
 const baseUrl = '/blog/posts';
 
-export default function IndexPage({ postData }) {
+export default function IndexPage({ articles }) {
   return (
     <Box>
       <Head>
@@ -20,20 +21,14 @@ export default function IndexPage({ postData }) {
 
       <Box py={8}>
         <Box m="1em">
-          <Box maxW="640px" mx="auto">
+          <Box maxW="960px" mx="auto">
             <Heading as="h1" mb={8}>
               Blog
             </Heading>
 
-            <UnorderedList mb={8}>
-              {postData.map((data) => (
-                <ListItem key={data.slug} py={2}>
-                  <Link href={`${baseUrl}/[slug]`} as={`${baseUrl}/${data.slug}`}>
-                    <ChakraLink>{data.frontMatter.title}</ChakraLink>
-                  </Link>
-                </ListItem>
-              ))}
-            </UnorderedList>
+            <Box mb={8}>
+              <ArticleList articles={articles} baseUrl={baseUrl} />
+            </Box>
 
             <Box mb={16}>
               <Link href="/">
@@ -51,13 +46,19 @@ export default function IndexPage({ postData }) {
 
 export async function getStaticProps() {
   const contentRoot = path.join(root, contentDir);
-  const postData = fs.readdirSync(contentRoot).map((p) => {
-    const content = fs.readFileSync(path.join(contentRoot, p), 'utf8');
+  const articles = fs.readdirSync(contentRoot).map((p) => {
+    const source = fs.readFileSync(path.join(contentRoot, p), 'utf8');
+    const { data, content } = matter(source);
+
     return {
       slug: p.replace(/\.mdx/, ''),
-      content,
-      frontMatter: matter(content).data,
+      title: data.title,
+      excerpt: content.substr(0, 128),
+      date: data.date || null,
+      image: data.hero || null,
+      tags: data.tags || null,
     };
   });
-  return { props: { postData } };
+
+  return { props: { articles } };
 }
