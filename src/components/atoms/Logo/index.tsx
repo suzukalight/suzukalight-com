46f6066b2aab93c4ce7994b9d1868de1e59f6700
suzukalight <mail@mkubara.com>
2020-12-14 23:06:09 +0900

    Add logo

--- src/components/atoms/Logo/index.tsx
+++ src/components/atoms/Logo/index.tsx
@@ -1,5 +1,13 @@
+import { Box, Avatar, Text } from '@chakra-ui/react';
 import React from 'react';
 
-export const Logo: React.FC = () => <></>;
+export const Logo: React.FC = () => (
+  <Box>
+    <Text fontSize="lg">
+      <Avatar borderRadius="full" boxSize={8} src="tarako.jpg" name="suzukalight" mr={3} />
+      suzukalight
+    </Text>
+  </Box>
+);
 
 export default Logo;
