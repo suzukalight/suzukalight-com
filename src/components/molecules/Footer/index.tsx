30b576617c8bf9602008e51b7779221cb41cbc0d
suzukalight <mail@mkubara.com>
2020-12-16 17:14:01 +0900

    Adjust html semantics

--- src/components/molecules/Footer/index.tsx
+++ src/components/molecules/Footer/index.tsx
@@ -1,42 +1,33 @@
 import React from 'react';
 import Link from 'next/link';
-import {
-  Flex,
-  Center,
-  Box,
-  Heading,
-  Link as ChakraLink,
-  SimpleGrid,
-  Divider,
-  Text,
-} from '@chakra-ui/react';
+import { Flex, Center, Box, Link as ChakraLink, SimpleGrid, Divider, Text } from '@chakra-ui/react';
 
 export const Footer: React.FC = () => (
   <Flex direction="column" alignItems="center" w="100%" backgroundColor="gray.100">
     <Box w="100%" maxW="80em" mt={12}>
       <SimpleGrid columns={[1, 2, 3]} gap={12}>
         <Box textAlign="center">
-          <Heading as="h4" size="sm">
+          <Text size="sm" fontWeight="bold">
             Writings
-          </Heading>
+          </Text>
           <Link href="/blog">
             <ChakraLink href="/blog">blog</ChakraLink>
           </Link>
         </Box>
 
         <Box textAlign="center">
-          <Heading as="h4" size="sm">
+          <Text size="sm" fontWeight="bold">
             Outputs
-          </Heading>
+          </Text>
           <Link href="/products">
             <ChakraLink href="/products">products</ChakraLink>
           </Link>
         </Box>
 
         <Box textAlign="center">
-          <Heading as="h4" size="sm">
+          <Text size="sm" fontWeight="bold">
             About
-          </Heading>
+          </Text>
           <Link href="/about">
             <ChakraLink href="/about">skill map</ChakraLink>
           </Link>
