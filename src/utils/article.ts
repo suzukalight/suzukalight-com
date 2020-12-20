4a8d3c8de08ce97d0074b841ba9dc1849e669616
suzukalight <mail@mkubara.com>
2020-12-20 17:04:49 +0900

    Add article utils about tag operation

--- src/utils/article.ts
+++ src/utils/article.ts
@@ -53,3 +53,65 @@ export const sortArticlesByDateDesc = (articles: ArticleData[]) => {
 export const getArticleDate = (date: string) => {
   return date ? format(new Date(date), 'yyyy.MM.dd') : '';
 };
+
+/**
+ * 指定したタグを含む記事のみを返す
+ * @param articles 記事
+ * @param tag
+ */
+export const filterArticleByTag = (articles: ArticleData[], tag: string) => {
+  return articles.filter((a) => a.tags.some((t) => t === tag));
+};
+
+/**
+ * 記事に含まれるタグ一覧を重複なしで返す
+ * @param articles 記事
+ */
+export const getTagsIncludedInArticles = (articles: ArticleData[]) => {
+  const tags = articles.reduce((p, c) => p.concat(c.tags), [] as string[]);
+  return Array.from(new Set(tags));
+};
+
+type TagAndCountTable = {
+  [key: string]: number;
+};
+
+/**
+ * { [tagName]: count } 形式でタグ一覧を返す
+ * @param articles
+ */
+export const getTableWithTagAndCountIncludedInArticles = (articles: ArticleData[]) => {
+  return articles
+    .map((a) => Array.from(new Set(a.tags)))
+    .reduce((p, tags) => {
+      tags.forEach((tag) => (tag in p ? p[tag]++ : (p[tag] = 1)));
+      return p;
+    }, {} as TagAndCountTable);
+};
+
+export type TagAndCount = {
+  tag: string;
+  count: number;
+};
+type ComparatorTagAndCount = (a: TagAndCount, b: TagAndCount) => number;
+
+export const comparatorTagName = (a: TagAndCount, b: TagAndCount) => a.tag.localeCompare(b.tag);
+export const comparatorTagCount = (a: TagAndCount, b: TagAndCount) => b.count - a.count;
+
+/**
+ * [{ tag: string, count: number }] 形式でタグ一覧を返す
+ * @param tagAndCountTable getTableWithTagAndCountIncludedInArticles の戻り値
+ * @param comparator ソート順を決定する comparator
+ */
+export const getArrayOfTagAndCountFromTable = (
+  tagAndCountTable: TagAndCountTable,
+  comparator: ComparatorTagAndCount,
+) => {
+  return Object.entries(tagAndCountTable)
+    .map((e) => ({
+      tag: e[0],
+      count: e[1],
+    }))
+    .sort(comparatorTagName)
+    .sort(comparator) as TagAndCount[];
+};
