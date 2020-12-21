4f1fc7b066b8f24c6e5b627272335174a629b079
suzukalight <mail@mkubara.com>
2020-12-21 23:28:43 +0900

    Add Article entity

---
+++ src/utils/article/entity.ts
@@ -0,0 +1,150 @@
+import matter from 'gray-matter';
+import format from 'date-fns/format';
+
+import { ValidationError } from '../errors/Validation';
+import { PropertyRequiredError } from '../errors/PropertyRequired';
+
+/** 記事URL */
+export const blogRootUrl = '/blog/posts';
+
+/** 記事を格納しているディレクトリ（public内） */
+export const blogContentsUrl = '/contents/blog';
+
+/** 記事の公開状態 */
+export type ArticleStatus = 'published' | 'draft';
+
+/** MDXの先頭に記述している frontMatter */
+export type ArticleFrontMatter = {
+  title: string;
+  date: string;
+  status: ArticleStatus;
+  category?: string;
+  tags?: string[];
+  hero?: string;
+  emoji?: string;
+};
+
+/** ArticleエンティティのDTO */
+export type ArticleDTO = {
+  frontMatter: ArticleFrontMatter;
+  content: ArticleContent;
+  slug: string;
+};
+
+/** MDXからfrontMatterを取り除いた本文 */
+export type ArticleContent = string;
+
+/**
+ * 不正な frontMatter であった場合、エラーを throw する
+ * @param frontMatter ArticleFrontMatter 形式のオブジェクト
+ */
+export const denyInvalidFrontMatter = (frontMatter: any) => {
+  if (!frontMatter) throw new ValidationError('frontMatterが見つかりません');
+  if (!frontMatter.title) throw new PropertyRequiredError('title');
+  if (!frontMatter.date) throw new PropertyRequiredError('date');
+  if (!frontMatter.status) throw new PropertyRequiredError('status');
+  if (frontMatter.tags && !Array.isArray(frontMatter.tags))
+    throw new ValidationError('tagsが配列ではありません');
+};
+
+/**
+ * 正しい形式の frontMatter であるかを返す
+ * @param frontMatter ArticleFrontMatter 形式のオブジェクト
+ */
+export const isValidFrontMatter = (frontMatter: any) => {
+  try {
+    denyInvalidFrontMatter(frontMatter);
+  } catch (e) {
+    return false;
+  }
+  return true;
+};
+
+/**
+ * 不正な DTO であった場合、エラーを throw する
+ * @param dto ArticleDTO 形式のオブジェクト
+ */
+export const denyInvalidArticleDTO = (dto: any) => {
+  if (!dto) throw new ValidationError('DTOが見つかりません');
+  if (!('frontMatter' in dto)) throw new PropertyRequiredError('frontMatter');
+  if (!('content' in dto)) throw new PropertyRequiredError('content');
+  if (!('slug' in dto)) throw new PropertyRequiredError('slug');
+
+  denyInvalidFrontMatter(dto.frontMatter);
+};
+
+/**
+ * MDX に含まれる frontmatterを分離し、Article として返す
+ * @param source MDXファイルの内容
+ * @param slug このArticleのslug
+ */
+export const getArticleFromMdxSource = (source: string, slug: string) => {
+  const { data, content } = matter(source);
+  denyInvalidFrontMatter(data);
+
+  return new Article(data as ArticleFrontMatter, content, slug);
+};
+
+/**
+ * 記事エンティティ
+ */
+export class Article {
+  private frontMatter: ArticleFrontMatter;
+  private content: ArticleContent;
+  private slug: string;
+
+  constructor(frontMatter: ArticleFrontMatter, content: ArticleContent, slug: string) {
+    this.frontMatter = frontMatter;
+    this.content = content;
+    this.slug = slug;
+  }
+
+  getFrontMatter() {
+    return this.frontMatter;
+  }
+
+  getContent() {
+    return this.content;
+  }
+
+  getSlug() {
+    return this.slug;
+  }
+
+  getExcerpt() {
+    return this.content.substr(0, 128);
+  }
+
+  /** DTOからエンティティを生成 */
+  static fromDTO(dto: any) {
+    denyInvalidArticleDTO(dto);
+    return new Article(dto.frontMatter, dto.content, dto.slug);
+  }
+
+  /** MDXデータとslugからエンティティを生成 */
+  static fromMdxSource(source: string, slug: string) {
+    return getArticleFromMdxSource(source, slug);
+  }
+
+  /** DTOに変換 */
+  toDTO() {
+    return {
+      frontMatter: this.frontMatter,
+      content: this.content,
+      slug: this.slug,
+    };
+  }
+
+  /** 記事が公開済みかどうか */
+  isPublished() {
+    return this.frontMatter.status === 'published';
+  }
+
+  /**
+   * 投稿日時を整形して返す
+   * @param dateFormat 日付フォーマット
+   */
+  getDateFormatted(dateFormat = 'yyyy.MM.dd') {
+    return format(new Date(this.frontMatter.date), dateFormat);
+  }
+}
