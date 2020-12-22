bf1ac90e39bc489331bfcad0d14c3551ce4195b1
suzukalight <mail@mkubara.com>
2020-12-22 23:01:02 +0900

    Update DefaultLayout styles

--- src/components/templates/DefaultLayout/index.tsx
+++ src/components/templates/DefaultLayout/index.tsx
@@ -1,18 +1,16 @@
 import React from 'react';
-import { Flex, Box } from '@chakra-ui/react';
+import { Flex, Box, ChakraProps } from '@chakra-ui/react';
 
 import { Header } from '../../molecules/Header';
 import { Footer } from '../../molecules/Footer';
 
-type DefaultLayoutProps = {
-  [key: string]: any;
-};
+type DefaultLayoutProps = ChakraProps;
 
 export const DefaultLayout: React.FC<DefaultLayoutProps> = ({ children, ...props }) => (
-  <Flex direction="column" align="center" m="0 auto" {...props}>
+  <Flex direction="column" align="center" minH="100vh" m="0 auto" {...props}>
     <Header />
 
-    <Box w="100%" maxW="80em" mt={16}>
+    <Box flexGrow={1} w="100%" maxW="80em" mt={16}>
       {children}
     </Box>
 
