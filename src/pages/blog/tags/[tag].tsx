ae5477e716843e68ac0a0ad5fc23568f9f697975
suzukalight <mail@mkubara.com>
2020-12-31 09:32:44 +0900

    Use ArticleListItem in tag page

--- src/pages/blog/tags/[tag].tsx
+++ src/pages/blog/tags/[tag].tsx
@@ -1,14 +1,14 @@
 import React from 'react';
 import { GetStaticPaths, GetStaticProps } from 'next';
-import { Box, Heading, Divider } from '@chakra-ui/react';
+import { Box, Heading, Divider, VStack, StackDivider } from '@chakra-ui/react';
 import { FaHome, FaPencilAlt } from 'react-icons/fa';
 
-import { ArticleList } from '../../../components/molecules/ArticleList';
+import { ArticleListItem } from '../../../components/molecules/ArticleListItem';
 import DefaultLayout from '../../../components/templates/DefaultLayout';
 import { HtmlHead } from '../../../components/atoms/HtmlHead';
 import { BackLinks } from '../../../components/molecules/BackLinks';
 
-import { urlContentsBlog, urlBlogPosts } from '../../url.json';
+import { urlContentsBlog, urlBlogPosts, urlBlogTags } from '../../url.json';
 import { Article, ArticleDTO } from '../../../utils/article/entity';
 import { getArticles } from '../../../utils/article/fs.server';
 import { getTagsIncludedInArticles } from '../../../utils/article/tag';
@@ -21,27 +21,32 @@ type IndexPageProps = {
 };
 
 export const TagPage: React.FC<IndexPageProps> = ({ tag, articles: articleDTOs }) => {
-  const title = `"${tag}"に関する記事一覧`;
+  const title = `#${tag} タグの付いた Blog`;
   const articles = articleDTOs.map((dto) => Article.fromDTO(dto));
 
   return (
-    <DefaultLayout backgroundColor="gray.50">
+    <DefaultLayout>
       <HtmlHead title={title} />
 
       <Box py={8}>
         <Box m="1em">
-          <Box maxW="64em" mx="auto">
+          <Box maxW="50em" mx="auto">
             <Heading as="h1" mb={12}>
               {title}
             </Heading>
 
-            <Box mb={8}>
-              <ArticleList
-                articles={articles}
-                urlBlogPosts={urlBlogPosts}
-                urlContentsBlog={urlContentsBlog}
-              />
-            </Box>
+            <VStack spacing={8} divider={<StackDivider borderColor="gray.200" />}>
+              {articles.map((article) => (
+                <ArticleListItem
+                  key={article.getSlug()}
+                  article={article}
+                  contentBaseUrl={`${urlContentsBlog}/${article.getSlug()}`}
+                  tagBaseUrl={urlBlogTags}
+                  postBaseUrl={urlBlogPosts}
+                  showContentLink
+                />
+              ))}
+            </VStack>
 
             <Divider mt={12} mb={8} />
 
