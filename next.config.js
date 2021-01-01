174b4a9e0a206bda63dd11007dd6e50558bf58e0
suzukalight <mail@mkubara.com>
2021-01-01 22:05:16 +0900

    Update redirects

--- next.config.js
+++ next.config.js
@@ -2,6 +2,16 @@ module.exports = {
   async redirects() {
     return [
       {
+        source: '/tags/:slug*',
+        destination: '/blog/tags/:slug*',
+        permanent: true,
+      },
+      {
+        source: '/tags',
+        destination: '/blog/tags',
+        permanent: true,
+      },
+      {
         source: '/:slug([0-9]{4}-[0-9]{2}-[0-9]{2}.*$)',
         destination: '/blog/posts/:slug',
         permanent: true,
