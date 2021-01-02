3252c8a500b069bd63caa4316607d7e4425c588b
suzukalight <mail@mkubara.com>
2021-01-02 21:33:04 +0900

    Add CenterMaxW component

---
+++ src/components/atoms/CenterMaxW/index.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import { Box, SpaceProps, LayoutProps } from '@chakra-ui/react';
+
+type CenterMaxWProps = {
+  margin?: SpaceProps['m'];
+  maxWidth?: LayoutProps['maxW'];
+};
+
+export const CenterMaxW: React.FC<CenterMaxWProps> = ({ children, margin, maxWidth }) => (
+  <Box py={8}>
+    <Box m={margin ?? '1em'}>
+      <Box maxW={maxWidth ?? '40em'} mx="auto">
+        {children}
+      </Box>
+    </Box>
+  </Box>
+);
