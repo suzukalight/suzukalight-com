57a89c044ba0aeb383f0fedae4a0901bc4211c62
suzukalight <mail@mkubara.com>
2021-01-02 15:03:25 +0900

    Add next-seo.config.js

---
+++ next-seo.config.js
@@ -0,0 +1,20 @@
+export default {
+  openGraph: {
+    type: 'website',
+    locale: 'ja_JP',
+    url: 'https://suzukalight.com/',
+    site_name: 'suzukalight.com',
+    images: [
+      {
+        url: 'https://suzukalight-com.vercel.app/tarako.jpg',
+        width: 180,
+        height: 180,
+        alt: 'tarako | og-image',
+      },
+    ],
+  },
+  twitter: {
+    handle: '@suzukalight',
+    cardType: 'summary_large_image',
+  },
+};
