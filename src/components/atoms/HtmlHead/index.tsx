8cdf778ffb987acf841fa24d460d4d9d9051657f
suzukalight <mail@mkubara.com>
2021-01-04 23:21:46 +0900

    Add env util

--- src/components/atoms/HtmlHead/index.tsx
+++ src/components/atoms/HtmlHead/index.tsx
@@ -3,6 +3,8 @@ import Head from 'next/head';
 import { NextSeo } from 'next-seo';
 import { NextSeoProps } from 'next-seo/lib/types';
 
+import { SITE_NAME, SITE_URL } from '../../../utils/env';
+
 export type SeoProps = {
   title?: string;
   description?: string;
@@ -11,14 +13,12 @@ export type SeoProps = {
 };
 type HtmlHeadProps = SeoProps;
 
-export const siteName = process.env.NEXT_PUBLIC_SITE_NAME || 'website';
-export const siteUrl = process.env.NEXT_PUBLIC_SITE_URL || 'https://example.com/';
 export const defaultDescription =
   'A website created by Masahiko Kubara(suzukalight). Web engineering snippets, Horse Racing and Gaming blog';
 
 export const HtmlHead: React.FC<HtmlHeadProps> = ({ title, description, url, image }) => {
   const seoProps: NextSeoProps = {
-    title: title ? `${title} | ${siteName}` : siteName,
+    title: title ? `${title} | ${SITE_NAME}` : SITE_NAME,
     description: description || defaultDescription,
     openGraph: {
       title,
@@ -26,10 +26,10 @@ export const HtmlHead: React.FC<HtmlHeadProps> = ({ title, description, url, ima
     },
   };
   if (url) {
-    seoProps.openGraph.url = new URL(url, siteUrl).toString();
+    seoProps.openGraph.url = new URL(url, SITE_URL).toString();
   }
   if (url && image) {
-    seoProps.openGraph.images = [{ url: new URL(image, siteUrl).toString() }];
+    seoProps.openGraph.images = [{ url: new URL(image, SITE_URL).toString() }];
   }
 
   return (
---
+++ src/utils/env/index.ts
@@ -0,0 +1,4 @@
+export const SITE_NAME = process.env.NEXT_PUBLIC_SITE_NAME ?? 'website';
+export const SITE_URL = process.env.NEXT_PUBLIC_SITE_URL ?? 'https://example.com/';
+
+export const TWITTER_ID = process.env.NEXT_PUBLIC_TWITTER_ID ?? '';
