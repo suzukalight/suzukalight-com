2f80a26b3927f13e05195624f257cd3079d7fbe7
suzukalight <mail@mkubara.com>
2021-01-04 23:22:06 +0900

    Add SocialLinks component

---
+++ src/components/atoms/SocialLinks/NormalList.tsx
@@ -0,0 +1,42 @@
+import React from 'react';
+import { Icon } from '@chakra-ui/react';
+import {
+  FacebookShareButton,
+  HatenaIcon,
+  HatenaShareButton,
+  LineShareButton,
+  PocketShareButton,
+  TwitterShareButton,
+} from 'react-share';
+
+import { SITE_URL, TWITTER_ID } from '../../../utils/env';
+import { FaFacebookF, FaGetPocket, FaLine, FaTwitter } from 'react-icons/fa';
+
+type SocialLinksProps = {
+  urlBlog: string;
+  title: string;
+};
+
+export const SocialLinks: React.FC<SocialLinksProps> = ({ urlBlog, title }) => {
+  const url = new URL(urlBlog, SITE_URL).toString();
+
+  return (
+    <>
+      <TwitterShareButton url={url} title={title} via={TWITTER_ID}>
+        <Icon as={FaTwitter} boxSize={6} fill="gray.500" />
+      </TwitterShareButton>
+      <FacebookShareButton url={url}>
+        <Icon as={FaFacebookF} boxSize={6} fill="gray.500" />
+      </FacebookShareButton>
+      <LineShareButton title={title} url={url}>
+        <Icon as={FaLine} boxSize={6} fill="gray.500" />
+      </LineShareButton>
+      <PocketShareButton title={title} url={url}>
+        <Icon as={FaGetPocket} boxSize={6} fill="gray.500" />
+      </PocketShareButton>
+      <HatenaShareButton title={title} url={url}>
+        <HatenaIcon size={32} round bgStyle={{ fill: 'transparent' }} iconFillColor="#718096" />
+      </HatenaShareButton>
+    </>
+  );
+};
