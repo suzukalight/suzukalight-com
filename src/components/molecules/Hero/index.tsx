88c7944eab96639dfc1279ff798c458474917ea8
suzukalight <mail@mkubara.com>
2021-01-05 14:40:47 +0900

    Add SlideFade to hero

--- src/components/molecules/Hero/index.tsx
+++ src/components/molecules/Hero/index.tsx
@@ -1,5 +1,15 @@
 import React from 'react';
-import { Center, Flex, Image, Heading, Stack, Text, ChakraProps } from '@chakra-ui/react';
+import {
+  Center,
+  Flex,
+  Box,
+  Image,
+  Heading,
+  Stack,
+  Text,
+  SlideFade,
+  ChakraProps,
+} from '@chakra-ui/react';
 
 import { SNSLinks } from '../../atoms/SNSLinks';
 
@@ -23,46 +33,48 @@ export const Hero = ({ title, subtitle, image, chakraProps }: HeroProps) => (
       mb={[8, 8, 16]}
       {...chakraProps}
     >
-      <Stack
-        spacing={4}
-        w={['100%', '100%', '45%']}
-        pl={[0, 0, 8, 16, 24]}
-        align={['center', 'center', 'flex-start']}
-      >
-        <Heading
-          as="h1"
-          size="xl"
-          fontWeight="bold"
-          color="teal.800"
-          textAlign={['center', 'center', 'left']}
-        >
-          {title}
-        </Heading>
+      <Box w={['100%', '100%', '45%']} pl={[0, 0, 8, 16, 24]}>
+        <SlideFade in offsetX="-4em" offsetY={0}>
+          <Stack spacing={4} align={['center', 'center', 'flex-start']}>
+            <Heading
+              as="h1"
+              size="xl"
+              fontWeight="bold"
+              color="teal.800"
+              textAlign={['center', 'center', 'left']}
+            >
+              {title}
+            </Heading>
 
-        <Text
-          as="em"
-          size="md"
-          color="teal.800"
-          opacity="0.8"
-          fontWeight="normal"
-          lineHeight={1.5}
-          textAlign={['center', 'center', 'left']}
-        >
-          {subtitle}
-        </Text>
+            <Text
+              as="em"
+              size="md"
+              color="teal.800"
+              opacity="0.8"
+              fontWeight="normal"
+              lineHeight={1.5}
+              textAlign={['center', 'center', 'left']}
+            >
+              {subtitle}
+            </Text>
 
-        <SNSLinks spacing={3} boxSize={6} />
-      </Stack>
+            <SNSLinks spacing={3} boxSize={6} />
+          </Stack>
+        </SlideFade>
+      </Box>
 
-      <Image
-        src={image}
-        alt="hero image"
-        fit="cover"
-        w={['100%', '100%', '55%']}
-        h={['12em', '16em', '20em']}
-        mb={[12, 12, 0]}
-        shadow="2xl"
-      />
+      <Box w={['100%', '100%', '55%']} h={['12em', '16em', '20em']} mb={[12, 12, 0]} shadow="2xl">
+        <SlideFade in offsetX="4em" offsetY={0}>
+          <Image
+            src={image}
+            alt="hero image"
+            fit="cover"
+            w="100%"
+            h={['12em', '16em', '20em']}
+            fallback={<Box w="100%" h={['12em', '16em', '20em']} backgroundColor="yellow.100" />}
+          />
+        </SlideFade>
+      </Box>
     </Flex>
   </Center>
 );
