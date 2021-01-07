9bd5cb84b2952e4d1835300aff16a9a2dbb04a56
suzukalight <mail@mkubara.com>
2021-01-07 19:03:49 +0900

    Add pickupArticles, Update article list styles

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -8,6 +8,7 @@ import { Link } from '../components/atoms/Link';
 import { Hero } from '../components/molecules/Hero';
 import { CenterMaxW } from '../components/atoms/CenterMaxW';
 import { SlickArticles } from '../components/organisms/SlickArticles';
+import { ArticleTipList } from '../components/molecules/ArticleTipList';
 
 import {
   urlBlogRoot,
@@ -18,15 +19,20 @@ import {
   urlContentsSnippet,
 } from './url.json';
 import { Article } from '../utils/article/entity';
-import { getArticles } from '../utils/article/fs.server';
+import { getArticle, getArticles } from '../utils/article/fs.server';
 import { sortArticlesByDateDesc } from '../utils/article/sorter';
 
 type HomePageProps = {
+  pickupArticles: Article[];
   blogArticles: Article[];
   snippetArticles: Article[];
 };
 
-export const HomePage: React.FC<HomePageProps> = ({ blogArticles, snippetArticles }) => {
+export const HomePage: React.FC<HomePageProps> = ({
+  pickupArticles,
+  blogArticles,
+  snippetArticles,
+}) => {
   return (
     <DefaultLayout>
       <HtmlHead />
@@ -44,17 +50,41 @@ export const HomePage: React.FC<HomePageProps> = ({ blogArticles, snippetArticle
               <Text as="span">Blog</Text>
             </Heading>
 
-            <Box>
+            <Box mb={8}>
+              <Heading as="h2" fontSize="xl">
+                <Text as="span">Pickup Articles</Text>
+              </Heading>
+
               <SlickArticles
-                articles={blogArticles}
+                articles={pickupArticles}
                 urlContents={urlContentsBlog}
                 urlPosts={urlBlogPosts}
               />
             </Box>
 
             <Box>
+              <Heading as="h2" mb={4} fontSize="xl">
+                <Text as="span">Recent Articles</Text>
+              </Heading>
+
+              {blogArticles.length > 0 ? (
+                <ArticleTipList
+                  articles={blogArticles}
+                  urlBlogPosts={urlBlogPosts}
+                  urlContentsBlog={urlContentsBlog}
+                />
+              ) : (
+                <Text as="small" color="gray.600">
+                  関連する記事は見つかりませんでした
+                </Text>
+              )}
+            </Box>
+
+            <Box>
               <Link to={urlBlogRoot}>
-                <Text textDecoration="underline">すべてのBlogを見る→</Text>
+                <Text textDecoration="underline" _hover={{ color: 'teal.500' }}>
+                  すべてのBlogを見る→
+                </Text>
               </Link>
             </Box>
           </VStack>
@@ -69,16 +99,24 @@ export const HomePage: React.FC<HomePageProps> = ({ blogArticles, snippetArticle
             </Heading>
 
             <Box>
-              <SlickArticles
-                articles={snippetArticles}
-                urlContents={urlContentsSnippet}
-                urlPosts={urlSnippetPosts}
-              />
+              {snippetArticles.length > 0 ? (
+                <ArticleTipList
+                  articles={snippetArticles}
+                  urlBlogPosts={urlSnippetPosts}
+                  urlContentsBlog={urlContentsSnippet}
+                />
+              ) : (
+                <Text as="small" color="gray.600">
+                  関連する記事は見つかりませんでした
+                </Text>
+              )}
             </Box>
 
             <Box>
               <Link to={urlSnippetRoot}>
-                <Text textDecoration="underline">すべてのSnippetを見る→</Text>
+                <Text textDecoration="underline" _hover={{ color: 'teal.500' }}>
+                  すべてのSnippetを見る→
+                </Text>
               </Link>
             </Box>
           </VStack>
@@ -97,8 +135,20 @@ export const getStaticProps: GetStaticProps = async () => {
   const blogArticles = await getArticles(urlContentsBlog);
   const blogArticlesSorted = sortArticlesByDateDesc(blogArticles).slice(0, 6);
 
+  const pickupArticles = await Promise.all(
+    [
+      '2020-12-31-2020-reflection',
+      '2020-12-30-react-tech-stack',
+      '2019-09-09-react-i18next',
+      '2019-09-06-join-carrot-club',
+      '2019-02-05-to-release-smartphone-app',
+      '2018-12-08-frontend-technology-selection',
+    ].map(async (slug) => await getArticle(slug, urlContentsBlog)),
+  );
+
   return {
     props: {
+      pickupArticles,
       blogArticles: blogArticlesSorted,
       snippetArticles: snippetArticlesSorted,
     } as HomePageProps,
