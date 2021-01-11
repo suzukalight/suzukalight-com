d88f9d1780f52346ae1467cb555a51d5724acf3e
suzukalight <mail@mkubara.com>
2021-01-11 09:50:15 +0900

    Add url utils and constants

---
+++ src/utils/path/url.ts
@@ -0,0 +1,12 @@
+export const UrlTable = {
+  blog: '/blog',
+  blogPosts: '/blog/posts',
+  blogTags: '/blog/tags',
+  textbook: '/textbook',
+  snippet: '/snippet',
+  snippetPosts: '/snippet/posts',
+  snippetTags: '/snippet/tags',
+  about: '/about',
+};
+
+export const getContentsUrl = (url: string) => `/contents${url}`;
