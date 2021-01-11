51f4060a489ba50226b0fbf242815f4df5b61d41
suzukalight <mail@mkubara.com>
2021-01-11 09:51:44 +0900

    Add filepath utils

---
+++ src/utils/path/file.server.ts
@@ -0,0 +1,19 @@
+import path from 'path';
+import fs from 'fs';
+
+import { getContentsUrl } from './url';
+
+const root = process.cwd();
+
+export const getContentsRootDir = (url: string) => {
+  return path.join(root, 'public', getContentsUrl(url));
+};
+
+export const getContentsDirNames = (url: string) => {
+  const contentsRootDir = path.join(root, 'public', url);
+  return fs.readdirSync(contentsRootDir);
+};
+
+export const getContentsDir = (slug: string, url: string) => {
+  return path.join(root, 'public', getContentsUrl(url), slug);
+};
