6437be2b09f6582bb6ba43bfd6e05a223382ebd8
suzukalight <mail@mkubara.com>
2021-01-11 09:59:47 +0900

    Update api

--- src/pages/api/blog/posts.ts
+++ src/pages/api/blog/posts.ts
@@ -1,12 +1,12 @@
 import { NextApiRequest, NextApiResponse } from 'next';
 
-import { urlContentsBlog } from '../../url.json';
 import { getArticles } from '../../../utils/article/fs.server';
 import { sortArticlesByDateDesc } from '../../../utils/article/sorter';
 import { stripContent } from '../../../utils/article/entity';
+import { UrlTable } from '../../../utils/path/url';
 
 export default async (req: NextApiRequest, res: NextApiResponse) => {
-  const articles = await getArticles(urlContentsBlog);
+  const articles = await getArticles(UrlTable.blog);
   const articlesSorted = sortArticlesByDateDesc(articles).map((a) => stripContent(a));
 
   res.statusCode = 200;
