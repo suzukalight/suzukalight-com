063a0d019d2267cdbeceee23c0617dab59c3795a
suzukalight <mail@mkubara.com>
2021-01-12 10:45:18 +0900

    Use AboutMe components on about page

--- src/pages/about/index.tsx
+++ src/pages/about/index.tsx
@@ -1,13 +1,14 @@
 import React from 'react';
 import { GetStaticProps } from 'next';
-import { StackDivider, Avatar, VStack, HStack, Box, Text } from '@chakra-ui/react';
+import { StackDivider, VStack, Box } from '@chakra-ui/react';
 import { FaHome } from 'react-icons/fa';
 
 import { ArticleListLayout } from '../../components/templates/ArticleListLayout';
 import { HtmlHead } from '../../components/molecules/HtmlHead';
 import { BackLinks } from '../../components/molecules/BackLinks';
+import { AboutMePhoto } from '../../components/molecules/AboutMePhoto';
+import { AboutMeCards } from '../../components/molecules/AboutMeCards';
 import { ArticleDetail } from '../../components/molecules/ArticleDetail';
-import { SNSLinks } from '../../components/molecules/SNSLinks';
 
 import { getArticle } from '../../utils/article/fs.server';
 import { hydrate } from '../../utils/article/markdown';
@@ -26,24 +27,8 @@ export const IndexPage: React.FC<IndexPageProps> = ({ contentHtml }) => {
       <HtmlHead title="About" url="/about" />
 
       <VStack spacing={8} align="left" divider={<StackDivider borderColor="gray.200" />}>
-        <VStack spacing={2} align="left">
-          <HStack justifyContent="center" align="center" w="100%" spacing={8} mb={8}>
-            <Avatar src="/images/masahiko_kubara.jpg" name="suzukalight" size="xl" />
-            <Avatar src="/images/tarako.jpg" name="suzukalight" size="xl" />
-          </HStack>
-
-          <HStack spacing={4}>
-            <Text display="inline-block" fontWeight="bold">
-              Masahiko Kubara (suzukalight)
-            </Text>
-
-            <SNSLinks spacing={3} boxSize={4} color="gray.800" />
-          </HStack>
-
-          <Text>
-            テックリード・スクラムマスター・フロントエンドエンジニア。修士（メディア科学）。
-          </Text>
-        </VStack>
+        <AboutMePhoto />
+        <AboutMeCards />
 
         <Box mt={-8}>
           <ArticleDetail contentHtml={content} />
