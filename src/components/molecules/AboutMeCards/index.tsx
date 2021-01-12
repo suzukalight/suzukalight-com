75a51acd7d2648a04375509080ee4e474e788a44
suzukalight <mail@mkubara.com>
2021-01-12 10:44:22 +0900

    Add AboutMeCards component

---
+++ src/components/molecules/AboutMeCards/index.tsx
@@ -0,0 +1,29 @@
+import React from 'react';
+import { SimpleGrid, Icon } from '@chakra-ui/react';
+import { FaCodeBranch, FaChalkboardTeacher } from 'react-icons/fa';
+import { GiHorseHead } from 'react-icons/gi';
+
+import { CardFrame } from '../../atoms/Card/Frame';
+
+export const AboutMeCards: React.FC = () => (
+  <SimpleGrid columns={[1, 2, 2, 3]} columnGap={12}>
+    <CardFrame
+      image={<Icon as={FaChalkboardTeacher} boxSize={12} />}
+      title="Teaching"
+      supplement="大学院で情報科学を修めており、大学および専門学校で教員として教えた経験があります。ほかに子ども向けの電子工作ワークショップや、地域住民向けのPC講座なども行ったことがあります。"
+      wrapProps={{ justifySelf: 'center', alignSelf: 'center' }}
+    />
+    <CardFrame
+      image={<Icon as={FaCodeBranch} boxSize={12} />}
+      title="Fullstack Engineering"
+      supplement="フロントエンド領域を中心とした、フルスタックエンジニアとして活動しています。React.js系のSPA開発を中心に、Node.jsバックエンド、React Nativeアプリ、AWSインフラなどが扱えます。"
+      wrapProps={{ justifySelf: 'center', alignSelf: 'center' }}
+    />
+    <CardFrame
+      image={<Icon as={GiHorseHead} boxSize={12} />}
+      title="Horse Racing"
+      supplement="競馬とゲームが趣味です。一口馬主として競馬に参加しています。高校生の頃に競馬予想アプリを作成・販売した経験があり、これがエンジニアとしての原体験になっています。"
+      wrapProps={{ justifySelf: 'center', alignSelf: 'center' }}
+    />
+  </SimpleGrid>
+);
