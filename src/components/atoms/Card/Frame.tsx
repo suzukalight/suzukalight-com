31ff5ca4a278b798b8de20d3db39a758e676d688
suzukalight <mail@mkubara.com>
2021-01-12 10:44:39 +0900

    Add CardFrame component

---
+++ src/components/atoms/Card/Frame.tsx
@@ -0,0 +1,29 @@
+import React from 'react';
+import { Center, Heading, StackProps, Text, VStack } from '@chakra-ui/react';
+
+type CardFrameProps = {
+  image: React.ReactNode;
+  title: React.ReactNode;
+  supplement: React.ReactNode;
+  wrapProps?: StackProps;
+};
+
+export const CardFrame: React.FC<CardFrameProps> = ({ image, title, supplement, wrapProps }) => {
+  return (
+    <VStack spacing={4} overflow="hidden" maxW={64} h={64} {...wrapProps}>
+      <Center flexShrink={0} w="100%" h={20}>
+        {image}
+      </Center>
+
+      <VStack spacing={2} align="center" w="100%" h={44} overflow="hidden">
+        <Heading as="h2" overflow="hidden" h={6} fontSize="md" lineHeight={1.5}>
+          {title}
+        </Heading>
+
+        <Text maxH={32} wordBreak="break-word" fontSize="sm">
+          {supplement}
+        </Text>
+      </VStack>
+    </VStack>
+  );
+};
