72b804bab28911d816d80fa5d787960872fd3867
suzukalight <mail@mkubara.com>
2021-01-15 14:50:47 +0900

    Update NextImage props

--- src/components/atoms/NextImage/index.tsx
+++ src/components/atoms/NextImage/index.tsx
@@ -1,15 +1,23 @@
-import React from 'react';
+import React, { CSSProperties } from 'react';
 import Image, { ImageProps } from 'next/image';
 
-type NextImageProps = {
+type NextImageProps = ImageProps & {
   src: string;
   alt?: string;
   width?: string;
   height?: string;
-  fit?: ImageProps['objectFit'];
+  divStyle?: CSSProperties;
 };
 
-export const NextImage: React.FC<NextImageProps> = ({ src, alt, width, height, fit, ...props }) => (
+export const NextImage: React.FC<NextImageProps> = ({
+  src,
+  alt,
+  width,
+  height,
+  objectFit,
+  divStyle,
+  ...props
+}) => (
   <div
     style={{
       display: 'flex',
@@ -20,8 +28,9 @@ export const NextImage: React.FC<NextImageProps> = ({ src, alt, width, height, f
       height: height ?? '16em',
       marginBottom: '1.75em',
       backgroundColor: '#f7fafc',
+      ...divStyle,
     }}
   >
-    <Image {...props} src={src} alt={alt ?? src} layout="fill" objectFit={fit ?? 'contain'} />
+    <Image {...props} src={src} alt={alt ?? src} layout="fill" objectFit={objectFit ?? 'contain'} />
   </div>
 );
