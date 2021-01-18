d0dc968de8178f8ffb82b643fdca19d3cdefdeda
suzukalight <mail@mkubara.com>
2021-01-18 11:27:02 +0900

    Update about

--- src/components/molecules/AboutMePhoto/index.tsx
+++ src/components/molecules/AboutMePhoto/index.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Avatar, VStack, HStack, Text } from '@chakra-ui/react';
+import { Avatar, VStack, HStack, Stack, Text } from '@chakra-ui/react';
 
 import { SNSLinks } from '../../molecules/SNSLinks';
 
@@ -10,16 +10,16 @@ export const AboutMePhoto: React.FC = () => (
       <Avatar src="/images/tarako.jpg" name="suzukalight" size="xl" />
     </HStack>
 
-    <HStack spacing={4}>
-      <Text display="inline-block" fontWeight="bold">
+    <Stack direction={['column', 'row']} spacing={[0, 4]} alignSelf="center" align="center">
+      <Text display="inline-block" fontSize="lg" fontWeight="bold">
         Masahiko Kubara (suzukalight)
       </Text>
 
       <SNSLinks spacing={3} boxSize={4} color="gray.800" />
-    </HStack>
+    </Stack>
 
     <Text>
-      Webエンジニア。CS系の修士号。高校在学中に競馬アプリの制作および販売を経験。大学ではGUI・ハードウェア・福祉工学を研究。就職後はテックリード、スクラムマスター、採用育成などを経験。趣味は競馬とゲーム。
+      Webエンジニア。CS系の修士号。高校在学中に自作の競馬アプリを制作および販売。大学ではGUI・ハードウェア・福祉工学を研究。就職後はテックリード、スクラムマスター、採用育成などを経験。趣味は競馬とゲーム。
     </Text>
   </VStack>
 );
