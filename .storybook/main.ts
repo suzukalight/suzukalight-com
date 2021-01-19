96913b9fedea5eda5cdbe6b5158645c884bee636
suzukalight <mail@mkubara.com>
2021-01-19 21:24:49 +0900

    Support CSS Modules in storybook

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -8,6 +8,7 @@ module.exports = {
     '@storybook/addon-links',
     '@storybook/addon-essentials',
     'storybook-addon-performance/register',
+    '@storybook/preset-scss',
   ],
   typescript: {
     check: false,
