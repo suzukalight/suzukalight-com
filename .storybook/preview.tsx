1009769689a8489be2e91b4c34bdf83febf63fdb
suzukalight <mail@mkubara.com>
2021-01-19 15:53:56 +0900

    Update configs

---
+++ .storybook/preview.tsx
@@ -0,0 +1,33 @@
+import * as React from 'react';
+import { ChakraProvider, extendTheme } from '@chakra-ui/react';
+import { StoryContext } from '@storybook/react';
+import { withPerformance } from 'storybook-addon-performance';
+
+/**
+ * Add global context for RTL-LTR switching
+ */
+export const globalTypes = {
+  direction: {
+    name: 'Direction',
+    description: 'Direction for layout',
+    defaultValue: 'LTR',
+    toolbar: {
+      icon: 'globe',
+      items: ['LTR', 'RTL'],
+    },
+  },
+};
+
+const withChakra = (StoryFn: Function, context: StoryContext) => {
+  const { direction } = context.globals;
+  const dir = direction.toLowerCase();
+  return (
+    <ChakraProvider theme={extendTheme({ direction: dir })}>
+      <div dir={dir} id="story-wrapper" style={{ minHeight: '100vh' }}>
+        <StoryFn />
+      </div>
+    </ChakraProvider>
+  );
+};
+
+export const decorators = [withChakra, withPerformance];
