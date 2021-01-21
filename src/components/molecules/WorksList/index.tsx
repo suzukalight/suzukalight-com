25abbce1fc05f6f9640f32dbef4e38918c3461eb
suzukalight <mail@mkubara.com>
2021-01-21 18:52:12 +0900

    Add WorksList component

---
+++ src/components/molecules/WorksList/index.tsx
@@ -0,0 +1,39 @@
+import React from 'react';
+import { Box, SimpleGrid } from '@chakra-ui/react';
+
+import { CardCatalog } from '../../atoms/Card/Catalog';
+import { NextImageOrEmoji } from '../../atoms/NextImage/ImageOrEmoji';
+import { Link } from '../../atoms/Link';
+
+import { Article } from '../../../utils/article/entity';
+import { getContentsUrlWithSlug, mergeUrlAndSlug, UrlTable } from '../../../utils/path/url';
+
+type WorksListProps = {
+  works: Article[];
+};
+
+export const WorksList: React.FC<WorksListProps> = ({ works }) => (
+  <SimpleGrid columns={[1, 1, 2, 3]} columnGap={4} rowGap={16} w="100%">
+    {works.map(({ frontMatter, slug }) => (
+      <Link key={slug} href={mergeUrlAndSlug(slug, UrlTable.workDetail)}>
+        <Box w="100%" maxH="20em">
+          <CardCatalog
+            image={
+              <NextImageOrEmoji
+                src={mergeUrlAndSlug(frontMatter.hero, getContentsUrlWithSlug(slug, UrlTable.work))}
+                emoji={frontMatter.emoji}
+                width="100%"
+                height="12em"
+                objectFit="cover"
+                divStyle={{ marginBottom: 0 }}
+              />
+            }
+            title={frontMatter.title}
+            supplement={frontMatter.supplement}
+            wrapProps={{ justifySelf: 'center', alignSelf: 'center' }}
+          />
+        </Box>
+      </Link>
+    ))}
+  </SimpleGrid>
+);
