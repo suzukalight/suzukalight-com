0ad4538de3a7d6ae826fadd1cff3d6e866914df5
suzukalight <mail@mkubara.com>
2021-01-23 22:29:26 +0900

    Add @next/bundle-analyzer package

--- next.config.js
+++ next.config.js
@@ -1,4 +1,8 @@
-module.exports = {
+const withBundleAnalyzer = require('@next/bundle-analyzer')({
+  enabled: process.env.ANALYZE === 'true',
+});
+
+module.exports = withBundleAnalyzer({
   async redirects() {
     return [
       {
@@ -18,4 +22,4 @@ module.exports = {
       },
     ];
   },
-};
+});
