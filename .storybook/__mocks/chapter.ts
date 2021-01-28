9c973c9dd7ad3474882ca4b402065b1d4eade317
suzukalight <mail@mkubara.com>
2021-01-28 22:04:56 +0900

    Add chapter mock

---
+++ .storybook/__mocks/chapter.ts
@@ -0,0 +1,30 @@
+export const chapter2 = {
+  slug: '02-first-programming',
+  excerpt:
+    '本章では下記の内容を学習します。　最初のプログラム： プログラムの作り方　printf 関数とエスケープシーケンス： 様々な文字列の出力方法　コンパイルとリンク： 動くプログラムを作り出す　デバッグと良くあるミス： プログラムを修正する　最初のプログラム　実行　コンピュータプログラムの業界で、最初に書くプログラムといえば、「Hello, World! という文字列を画面に表示する」で、これができればプログラミン\n',
+  frontMatter: {
+    title: '初めてのC言語プログラミング',
+    date: '2012-05-01T00:02:00',
+    status: 'published',
+  },
+};
+
+export const chapter3 = {
+  slug: '03-variable-datatype',
+  excerpt:
+    '本章では下記の内容を学習します。　変数： 変数とは、数学での変数との違い　データ型： データ型とは、整数型・浮動小数点型・文字型　変数の宣言： 宣言、代入、初期化　変数の値の表示： printf 関数による変数値の表示　変数　変数とは　',
+  frontMatter: { title: '変数とデータ型', date: '2012-05-01T00:03:00', status: 'published' },
+};
+
+export const chapter5 = {
+  slug: '05-scanf',
+  excerpt: '本章では下記の内容を学習します。　データの入力:　 scanf_s 関数　scanf_s 関数　',
+  frontMatter: { title: '値の入力（scanf_s）', date: '2012-05-01T00:05:00', status: 'published' },
+};
+
+export const chapter6 = {
+  slug: '06-if',
+  excerpt:
+    '本章では下記の内容を学習します。　条件分岐:　 if, else, else if　関係演算:　等しい、等しくない、小さい、大きい、以下、以上　論理関係:　かつ、または、否定　if 文による条件判断　if 文（もし ○○ ならば）　',
+  frontMatter: { title: '条件分岐（if文）', date: '2012-05-17T00:05:00', status: 'published' },
+};
