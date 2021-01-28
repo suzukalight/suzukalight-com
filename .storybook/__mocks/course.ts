bdc44d221b4d11c8a8a5d086f69af5724cc54e23
suzukalight <mail@mkubara.com>
2021-01-28 23:03:08 +0900

    Add course mock

---
+++ .storybook/__mocks/course.ts
@@ -0,0 +1,11 @@
+export const course1 = {
+  slug: 'clang',
+  excerpt:
+    'C 言語プログラミングでつまづくポイントを中心に、条件式（if/switch）、制御構文（for/while）、関数、変数、配列、文字列、そしてポインタなどの知識について、多くの実践問題を通して学んでいくコースです。\n',
+  frontMatter: {
+    title: '実践問題で学ぶ C言語プログラミング',
+    date: '2012-05-01T00:00:00',
+    hero: 'splash.png',
+    status: 'published',
+  },
+};
