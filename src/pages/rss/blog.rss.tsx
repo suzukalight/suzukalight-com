27477e64bf55d1aa79033048f01565765e4d467f
suzukalight <mail@mkubara.com>
2021-02-22 00:38:43 +0900

    Add blog rss

---
+++ src/pages/rss/blog.rss.tsx
@@ -0,0 +1,28 @@
+import React from 'react';
+import { GetServerSideProps } from 'next';
+
+import { UrlTable } from '../../utils/path/url';
+import { createRSSFeed, genRSSDataFromSlugs } from '../../utils/rss';
+
+export const getServerSideProps: GetServerSideProps = async ({ res }) => {
+  const rssBlog = await genRSSDataFromSlugs(UrlTable.blog, UrlTable.blogPosts);
+  const rssData = [...rssBlog];
+
+  const feed = createRSSFeed();
+  rssData?.forEach((r) => feed.item(r));
+
+  if (res) {
+    res.statusCode = 200;
+    res.setHeader('Cache-Control', 's-maxage=86400, stale-while-revalidate'); // 24時間キャッシュする
+    res.setHeader('Content-Type', 'text/xml');
+    res.end(feed.xml());
+  }
+
+  return {
+    props: {},
+  };
+};
+
+const Sitemap: React.FC = () => null;
+
+export default Sitemap;
