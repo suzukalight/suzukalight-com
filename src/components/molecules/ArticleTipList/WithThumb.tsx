e4b65a4902f1676aab35f77cbb17dc0648c8fbf4
suzukalight <mail@mkubara.com>
2025-03-02 14:19:52 +0900

    refactor: ArticleTipWithThumb コンポーネントのリンクと UI を改善

--- src/components/molecules/ArticleTipList/WithThumb.tsx
+++ src/components/molecules/ArticleTipList/WithThumb.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import Link from 'next/link';
 import Image from 'next/image';
-import { Box, Flex, Center, Text, SimpleGrid, Link as ChakraLink } from '@chakra-ui/react';
+import { Box, Flex, Center, Text, SimpleGrid } from '@chakra-ui/react';
 
 import styles from './WithThumb.module.scss';
 
@@ -20,54 +20,58 @@ export const ArticleTipWithThumb: React.FC<ArticleTipWithThumbProps> = ({ articl
   const { title, hero, emoji, tags } = article.frontMatter;
 
   return (
-    <Link href={`${url}/[slug]`} as={`${url}/${slug}`}>
-      <ChakraLink overflow="hidden" href={`${url}/${slug}`}>
-        <Flex direction="row" minH={10} maxH={24} overflow="hidden">
-          <Box flexShrink={0} mt={1} mr={4}>
-            {hero ? (
-              <Flex position="relative" justifyContent="center" alignItems="center">
-                <Image
-                  src={`${getContentsUrl(stripPosts(url))}/${slug}/${hero}`}
-                  alt={slug}
-                  width={32}
-                  height={32}
-                  style={{ objectFit: 'cover' }}
-                  className={styles.image}
-                />
-              </Flex>
-            ) : (
-              <Center boxSize={8}>
-                <Text fontSize="28px">{emoji ?? '📝'}</Text>
-              </Center>
-            )}
-          </Box>
-
-          <Flex flexGrow={1} direction="column">
-            <Text
-              as="p"
-              overflowY="hidden"
-              maxH="2.5em"
-              mb={1}
-              fontSize="sm"
-              fontWeight="600"
-              lineHeight={1.25}
-              // textDecoration="underline solid #22543D40"
-              wordBreak="break-all"
-              color="teal.800"
-            >
-              {title}
-            </Text>
-
-            <Flex flexGrow={1} maxH={4} overflow="hidden">
-              <TagListPlainText
-                tags={tags}
-                tagWrapProps={{ mb: 0 }}
-                tagItemProps={{ fontSize: 'xs', color: 'gray.400', lineHeight: 1.25 }}
+    <Link href={`${url}/${slug}`} style={{ textDecoration: 'none' }}>
+      <Flex
+        direction="row"
+        minH={10}
+        maxH={24}
+        overflow="hidden"
+        cursor="pointer"
+        _hover={{ textDecoration: 'none' }}
+      >
+        <Box flexShrink={0} mt={1} mr={4}>
+          {hero ? (
+            <Flex position="relative" justifyContent="center" alignItems="center">
+              <Image
+                src={`${getContentsUrl(stripPosts(url))}/${slug}/${hero}`}
+                alt={slug}
+                width={32}
+                height={32}
+                style={{ objectFit: 'cover' }}
+                className={styles.image}
               />
             </Flex>
+          ) : (
+            <Center boxSize={8}>
+              <Text fontSize="28px">{emoji ?? '📝'}</Text>
+            </Center>
+          )}
+        </Box>
+
+        <Flex flexGrow={1} direction="column">
+          <Text
+            as="p"
+            overflowY="hidden"
+            maxH="2.5em"
+            mb={1}
+            fontSize="sm"
+            fontWeight="600"
+            lineHeight={1.25}
+            wordBreak="break-all"
+            color="teal.800"
+          >
+            {title}
+          </Text>
+
+          <Flex flexGrow={1} maxH={4} overflow="hidden">
+            <TagListPlainText
+              tags={tags}
+              tagWrapProps={{ mb: 0 }}
+              tagItemProps={{ fontSize: 'xs', color: 'gray.400', lineHeight: 1.25 }}
+            />
           </Flex>
         </Flex>
-      </ChakraLink>
+      </Flex>
     </Link>
   );
 };
