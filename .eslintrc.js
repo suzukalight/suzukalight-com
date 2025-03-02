73f670f06a4f7d5d22df434815fe1efc58cdc32e
suzukalight <mail@mkubara.com>
2025-03-02 13:45:06 +0900

    fix: update TypeScript, ESLint and type definitions for Next.js 14 compatibility

--- .eslintrc.js
+++ .eslintrc.js
@@ -5,7 +5,6 @@ module.exports = {
     'plugin:@typescript-eslint/eslint-recommended',
     'plugin:@typescript-eslint/recommended',
     'prettier',
-    'prettier/@typescript-eslint',
   ],
   plugins: ['@typescript-eslint'],
   env: {
